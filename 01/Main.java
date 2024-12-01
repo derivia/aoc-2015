84020cfc48754d8813dc5da861e68b922bc906bd
derivia <contato.lorransoares@gmail.com>
2024-12-01 06:44:47 -0300

    chore: removed useless variables

--- 01/Main.java
+++ 01/Main.java
@@ -13,19 +13,15 @@ class Main {
     String line = read_file("input.txt");
 
     int floor = 0;
-    int count_down = 0;
-    int count_up = 0;
     Object[] entered = {0, false};
     for (int i = 0; i < line.length(); i++) {
       if (line.charAt(i) == '(') {
         floor += 1;
-        count_up += 1;
       } else if (line.charAt(i) == ')') {
         if (floor == 0 && (Boolean) entered[1] == false) {
           entered[0] = i + 1;
           entered[1] = true;
         }
-        count_down += 1;
         floor -= 1;
       }
     }
